fadcea1a3f6a2fcf0a4cfd8777c72f27e9eb2f3c
nathanarmstrong <nathan.armstrong21@gmail.com>
2017-06-07 22:07:47 +0000

    add 3 new stores and show how many of those stores cater to men

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,13 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+
+
+Store.create(name: 'Surry', annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: 'Whistler', annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: 'Yaletown', annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+
+@men = Store.where(mens_apparel: true)
+
+pp "there are #{@men.count} stors that cater to men."
