4ecdf99fb782c2abba47562fb2d56bc395fa08d0
nathanarmstrong <nathan.armstrong21@gmail.com>
2017-06-07 20:47:19 +0000

    find store one and change its name to Grabs, and find store 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,10 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.find(1)
+@store1.name = "Grabs"
+@store2 = Store.find(2)
+
+
+
+puts "#{@store1.name} has a new name, unlike #{@store2.name}"
