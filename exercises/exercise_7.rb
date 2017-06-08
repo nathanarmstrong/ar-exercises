dd058583557ff04c94fd51493b01bdb91e16e299
nathanarmstrong <nathan.armstrong21@gmail.com>
2017-06-08 06:59:35 +0000

    you can add a store and if content dose not fit it will through a error

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,48 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+
+pp "enter a name"
+@input_name = $stdin.gets.chomp
+
+pp "enter a revenue"
+@input_revenue = $stdin.gets.chomp
+
+pp "do you sell mens clothes"
+@input_man = $stdin.gets.chomp
+@upcase_man = @input_man.upcase!
+if @upcase_man === "YES"
+    @sell_man = true
+else
+    @sell_man = false
+end
+
+pp "do you sell womens clothes"
+@input_woman = $stdin.gets.chomp
+@upcase_woman = @input_woman.upcase!
+if @upcase_woman === "YES"
+    @sell_woman = true
+else
+    @sell_woman = false
+end
+
+@new_store = Store.create(name: @input_name, annual_revenue: @input_revenue, mens_apparel: @sell_man, womens_apparel: @sell_woman)
+@new_store.valid?
+@new_store.errors.messages.each {|message| pp message}
+pp "#{Store.count}"
+
+@created_store = Store.last.name
+
+pp "#{@created_store} is the newest store"
+@new_store.valid?
+@new_store.errors.messages.each {|message| pp message}
+
+
+
+
+
+
+
+
+
+
